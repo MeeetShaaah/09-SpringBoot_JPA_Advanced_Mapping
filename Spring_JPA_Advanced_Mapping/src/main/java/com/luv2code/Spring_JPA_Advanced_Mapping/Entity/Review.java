4e12417d5d16526c998c716ed824f9f4256e8f71
Meet Shah <m.shah@student.fdu.edu>
2023-12-14 11:48:22 -0500

    Start With one-to-many-uni coding.

---
+++ Spring_JPA_Advanced_Mapping/src/main/java/com/luv2code/Spring_JPA_Advanced_Mapping/Entity/Review.java
@@ -0,0 +1,10 @@
+package com.luv2code.Spring_JPA_Advanced_Mapping.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "review")
+public class Review {
+    private int
+}
